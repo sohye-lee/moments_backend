a6d7c80d2d95c0a17ad6f9d49eb50a46c1132c62
sohye-lee <sohyelee@gmail.com>
2021-03-18 01:19:25 -0400

    prisma postgresql

---
+++ server.js
@@ -0,0 +1,44 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+    type Movie {
+        title: String
+        year: Int
+    }
+
+    type Query {
+        movies: [Movie]
+        movie: Movie
+    }
+
+    type Mutation {
+        createMovie(title:String!): Boolean
+        deleteMovie(title:String!): Boolean
+    }
+`;
+
+const resolvers = {
+    Query: {
+        movies: () => [],
+        movie: () => ({ "title": "hello", "year": 2021}),
+    },
+    Mutation: {
+        createMovie: (_, {title}) => {
+            console.log(title);
+            return true;
+        },
+        deleteMovie: (_, {title}) => {
+            console.log(title);
+            return true;
+        },
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+server
+.listen()
+.then(() => console.log("Server is now running on http://localhost:4000/"));
